43e8d0c9a710253daccf8411a4c5c2662ad25bb7
Александр Степанов <ab3000645@gmail.com>
2022-02-28 14:49:42 +0300

    Algorithm was implemented

--- algorithm.js
+++ algorithm.js
@@ -1,9 +1,125 @@
+var alpha = 1.0;
+var beta = 1.0
+var q = 100;
+var p = 0.1;
+var startPheramones = 0.5;
+var antsCount = 10;
+
+var pointsCount = 0;
+var distances = [];
+var invDistances = [];
+var pheromones = [];
+var theShortestPath = null;
+
+function CreatePath(points, len)
+{
+    return {points, len};
+}
+
+function AddPointToPath(path, pointIndex)
+{
+    path.len += distances[pointIndex][path.points[path.points.length-1]];
+    path.points.push(pointIndex);
+}
+
 function ResetAlgorithmData(points)
 {
+    pointsCount = points.length;
+    distances = [];
+    invDistances = [];
+    pheromones = [];
+    for(let i = 0; i < pointsCount; i++)
+    {
+        let distRow = [];
+        let invRow = [];
+        let phRow = [];
+        for(let j = 0; j < pointsCount; j++)
+        {
+            let distance = Math.sqrt((points[i].x-points[j].x)*(points[i].x-points[j].x)+(points[i].y-points[j].y)*(points[i].y-points[j].y));
+            distRow.push(distance);
+            invRow.push(1/distance);
+            phRow.push(startPheramones);
+        }
+        distances.push(distRow);
+        invDistances.push(invRow);
+        pheromones.push(phRow);
+    }
+
+    theShortestPath = CreatePath([], 999999);
+}
+
+function CalculateChances(currentPoint, avaiblePoints)
+{
+    let chances = [];
+    let sum = 0;
+    for(let i = 0; i < avaiblePoints.length; i++)
+    {
+        let chance = Math.pow(pheromones[currentPoint][avaiblePoints[i]], alpha)*Math.pow(invDistances[currentPoint][avaiblePoints[i]], beta);
+        sum += chance;
+        chances.push(chance);
+    }
+    for(let i = 0; i < avaiblePoints.length; i++)
+        chances[i] /= sum;
     
+    return chances;
+}
+
+function SelectNext(chances)
+{
+    let rnd = Math.random();
+    let cur = 0;
+    let index = 0;
+    while(index < chances.length)
+    {
+        cur += chances[index];
+        if(cur >= rnd)
+            return index;
+        index++;
+    }
+    return chances.length-1;
+}
+
+function ReleaseAnAnt(startPoint)
+{
+    let avaiblePoints = [];
+    for(let i = 0; i < pointsCount; i++)
+        avaiblePoints.push(i);
+    avaiblePoints.splice(startPoint, 1);
+
+    let path = CreatePath([startPoint], 0);
+    
+    while(avaiblePoints.length > 0)
+    {
+        let chances = CalculateChances(path.points[path.points.length-1], avaiblePoints);
+        let next = SelectNext(chances);
+        AddPointToPath(path, avaiblePoints[next]);
+        avaiblePoints.splice(next, 1);
+    }
+    
+    AddPointToPath(path, startPoint);
+    return path;
 }
 
 function Iterate()
 {
+    let pathes = [];
+    for(let i = 0; i < antsCount; i++)
+        pathes.push(ReleaseAnAnt(Math.floor(Math.random()*pointsCount)));
 
+    for(let i = 0; i < pointsCount; i++)
+        for(let j = 0; j < pointsCount; j++)
+            pheromones[i][j] *= (1-p);
+
+    for(let i = 0; i < antsCount; i++)
+    {
+        let path = pathes[i];
+        let delta = q/path.len;
+        if(path.len < theShortestPath.len)
+            theShortestPath = path;
+        for(let j = 1; j < path.points.length; j++)
+        {
+            pheromones[path.points[j-1]][path.points[j]] += delta;
+            pheromones[path.points[j]][path.points[j-1]] += delta;
+        }
+    }
 }
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const SCENE_WIDTH = 1000;
+const SCENE_WIDTH = 1500;
 const SCENE_HEIGHT = 900;
 const POINT_SIZE = 30;
 const POINT_BORDER = 10;
 const POINT_COLOR = "black";
 const POINT_BORDER_COLOR = "red";
 const BACKGROUND_COLOR = "#F0F0F0";
+const LINE_COLOR = "black";
+const THE_SHORTEST_PATH_COLOR = "#00DD00";
 
 const scene = document.getElementById("scene");
 scene.width = SCENE_WIDTH;
@@ -25,10 +27,10 @@ function DrawPoint(x, y)
     ctx.fillRect(x - POINT_SIZE*0.5, y-POINT_SIZE*0.5, POINT_SIZE, POINT_SIZE);
 }
 
-function DrawLine(x1, y1, x2, y2, w)
+function DrawLine(x1, y1, x2, y2, w, color)
 {
     ctx.lineWidth = w;
-    ctx.strokeStyle = POINT_COLOR;
+    ctx.strokeStyle = color;
     ctx.beginPath();
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
@@ -37,11 +39,72 @@ function DrawLine(x1, y1, x2, y2, w)
 
 //////////////////////////////////////////////////////////////////////
 
+const EDIT_MODE = 0;
+const CALCULATION_MODE = 1;
+
+var mode = EDIT_MODE;
+var algorithmLoop = null;
+var iterationCount = 0;
+
+function IterateAlgorithm()
+{
+    iterationCount++;
+    iterationText.innerHTML = "iteration: " + iterationCount;
+    Iterate();
+    theShortestPathText.innerHTML = "the shortest path: " + theShortestPath.len;
+    Clear();
+    for(let i = 1; i < els.length; i++)
+        DrawLine(els[theShortestPath.points[i]].x, els[theShortestPath.points[i]].y, els[theShortestPath.points[i-1]].x, els[theShortestPath.points[i-1]].y, 18, THE_SHORTEST_PATH_COLOR);
+    DrawLine(els[theShortestPath.points[0]].x, els[theShortestPath.points[0]].y, els[theShortestPath.points[els.length-1]].x, els[theShortestPath.points[els.length-1]].y, 18, THE_SHORTEST_PATH_COLOR);
+    for(let i = 0; i < els.length; i++)
+        for(let j = 0; j < i; j++)
+            DrawLine(els[i].x, els[i].y, els[j].x, els[j].y, pheromones[i][j]*3, LINE_COLOR);
+    for(let i = 0; i < els.length; i++)
+        DrawPoint(els[i].x, els[i].y);
+    algorithmLoop = setTimeout(IterateAlgorithm, 250);
+}
+
+function ChangeMode()
+{
+    if(mode == EDIT_MODE && els.length >= 3)
+    {
+        mode = CALCULATION_MODE;
+        ResetAlgorithmData(els);
+        iterationCount = 0;
+        IterateAlgorithm();
+    }
+    else
+    {
+        mode = EDIT_MODE;
+        clearTimeout(algorithmLoop);
+    }
+}
+
+//////////////////////////////////////////////////////////////////////
+
+const pointsCountText = AddText("points count: 0");
+const theShortestPathText = AddText("the shortest path: 0");
+const iterationText = AddText("iteration: 0");
+const startButton = AddButton("start", e => {
+    ChangeMode();
+    startButton.innerHTML = (mode == EDIT_MODE)? "start" : "stop";
+});
+const alphaSlider = AddSlider("alpha", 0, 5, alpha, 0.1, e => alpha = alphaSlider.value);
+const betaSlider = AddSlider("beta", 0, 5, beta, 0.1, e => beta = betaSlider.value);
+const qSlider = AddSlider("q", 10, 100, q, 5, e => q = qSlider.value);
+const pSlider = AddSlider("p", 0.1, 0.5, p, 0.05, e => p = pSlider.value);
+const antsCountSlider = AddSlider("ants count", 10, 20, antsCount, 1, e => antsCount = antsCountSlider.value);
+
+//////////////////////////////////////////////////////////////////////
+
 const els = [];
 
 Clear();
 
 scene.onclick = (e) => {
+    if(mode != EDIT_MODE)
+        return;
+    
     let x = e.offsetX;
     let y = e.offsetY;
     let f = true;
@@ -57,12 +120,8 @@ scene.onclick = (e) => {
     Clear();
     for(let i = 0; i < els.length; i++)
         for(let j = 0; j < i; j++)
-            DrawLine(els[i].x, els[i].y, els[j].x, els[j].y, 2);
+            DrawLine(els[i].x, els[i].y, els[j].x, els[j].y, 2, LINE_COLOR);
     for(let i = 0; i < els.length; i++)
         DrawPoint(els[i].x, els[i].y);
+    pointsCountText.innerHTML = "points count: " + els.length;
 };
-
-
-let test = AddSlider("123456789", 0, 20, 10, 1, e => {});
-let btn = AddButton("123456789", e => btn.innerHTML = test.value);
-AddText("123456789");
